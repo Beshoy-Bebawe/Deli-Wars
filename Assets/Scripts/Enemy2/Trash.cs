e4a7923eb71e2dc6e0a175bb2fb552245377aadc
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2025-04-24 14:53:41 -0400

    Changes

--- Assets/Scripts/Enemy2/Trash.cs
+++ Assets/Scripts/Enemy2/Trash.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class Trash : MonoBehaviour
 {
     public GameObject trashPrefab;
+    private float spawnRate = 20.0f;
+
+   // public List<GameObject> targets; 
+    //private bool act;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+        Debug.Log("ok");
     }
-
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(trashPrefab, transform.position, trashPrefab.transform.rotation);
-        }
+
     }
+    IEnumerator SpawnTarget()
+      {
+         Debug.Log("Cool");
+           yield return new WaitForSeconds(spawnRate);
+          // int index = Random.Range(0, targets.Count);
+           //Instantiate(targets[index], transform.position, transform.rotation);
+           Instantiate(trashPrefab, transform.position, trashPrefab.transform.rotation);
+      }
 }
