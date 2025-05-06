c616d843adfe47fc8d33b047707846c35dded340
Gravityguy15 <113185554+Gravityguy15@users.noreply.github.com>
2025-05-06 13:31:18 -0400

    My Changes

--- Assets/Scripts/Enemy2/NinjaRat.cs
+++ Assets/Scripts/Enemy2/NinjaRat.cs
@@ -5,9 +5,9 @@ public class NinjaRat : MonoBehaviour
 {
     public ScriptObj Rat;
     public GameObject player;
-    private float speed;
-    private float health;
-    private float strength;
+    private int speed;
+    private int health;
+    private int strength;
     private float distanceBetween;
     bool LOS = false;
     float distance;
@@ -16,7 +16,16 @@ public class NinjaRat : MonoBehaviour
     Animator anim;
     Rigidbody2D rb;
     public LayerMask detect;
-    public Transform raycastOrigin;
+    PlayerDamTest playerD;
+    public Vector2 direction;
+    SpriteRenderer rend;
+    int currentHealth;
+    float kbTime = 4f;
+    public bool canKnock = true;
+    public bool knocked;
+    float kbPow = 6f;
+    float kbCD = 1f;
+    public bool Invic = false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,7 +34,8 @@ public class NinjaRat : MonoBehaviour
         strength = Rat.strength;
         distanceBetween = Rat.distanceBetween;
         Debug.Log(speed + " " +health+ " " + strength);
-
+        currentHealth = health;
+        rend = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -34,54 +44,60 @@ public class NinjaRat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (knocked)
+        {
+            return;
+        }   
+        
         distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.position;
+        direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg;
         if ((distance< distanceBetween) && (LOS))
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position,speed * Time.deltaTime);
+            rb.position = Vector2.MoveTowards(transform.position, player.transform.position,speed * 3 * Time.deltaTime);
             if (transform.position.x > player.transform.position.x - transform.position.x )
                 {
-                anim.SetBool("Direction",true);
+                    anim.SetBool("Direction", true);
                 }
                 else
                 {
-                anim.SetBool("Direction",false);
+                    anim.SetBool("Direction", false);
                 }
         }
-        else
-        {
-           timer -= Time.deltaTime;
-        if (timer <= 0)
-        {
-            Xdirection = Random.Range(-1,2);
-            timer = 3;
-        }
-        if (Xdirection == -1 && timer !=0)
-        {
+        // else
+        // {
+        //    timer -= Time.deltaTime;
+        // if (timer <= 0)
+        // {
+        //     Xdirection = Random.Range(-1,2);
+        //     timer = 3;
+        // }
+        // if (Xdirection == -1 && timer !=0)
+        // {
             
-            rb.velocity = Vector2.left * speed;
-            anim.SetBool("Direction",true);
-        }
-        else if (Xdirection ==1 && timer !=0)
-        {
+        //     rb.velocity = Vector2.left * speed;
+        //     anim.SetBool("Direction",true);
+        // }
+        // else if (Xdirection ==1 && timer !=0)
+        // {
             
-            rb.velocity = Vector2.right * speed;
-            anim.SetBool("Direction",false);
-        }
-        else
-        {
-        }
-        }
+        //     rb.velocity = Vector2.right * speed;
+        //     anim.SetBool("Direction",false);
+        // }
+        // else
+        // {
+        // }
+        // }
     }
       void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position,distanceBetween);
     }
-    void FixedUpdate()
+    void FixedUpdate() 
     {
-        // Calculate direction from this object to the player
+        // Calculate direction from this object to the playerw
         Vector2 direction = player.transform.position - transform.position;
         // Cast the ray toward the player, using the inverse of detect layermask
         RaycastHit2D ray = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, ~detect);
@@ -100,6 +116,68 @@ public class NinjaRat : MonoBehaviour
             }
         }
     }
+    
+    void OnCollisionStay2D(Collision2D other)
+    {
+        playerD = other.gameObject.GetComponent<PlayerDamTest>();
+        Vector2 direction = player.transform.position - transform.position;
+        
+        if ((playerD != null)  && playerD.Invic == false )
+        {
+            FindObjectOfType<HitStop>().Stop(0.2f);
+            playerD.ChangeHealth(-1);
+            StartCoroutine(HitStop());
+        }
+    }
+
+    public void TakeDamage(int damage) 
+    {
+        if (canKnock)
+        {
+        currentHealth = Mathf.Clamp(currentHealth + damage,0,health);
+        
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator HitStop()
+    {
+        while(Time.timeScale != 1.0f)
+        yield return null;
+        StartCoroutine(playerD.Knockback());
+    }
+    
+    public IEnumerator Knockback()
+    {
+        canKnock = false;
+        knocked = true;
+        rb.velocity = -direction * kbPow;
+        rend.color = Color.red;
+        yield return new WaitForSeconds(.25f);
+        knocked = false;
+        rend.color = Color.white;
+        rb.velocity  = Vector2.zero;
+        yield return new WaitForSeconds(kbCD);
+        canKnock = true;
+
+
+    }
+
+   
+
+}
 
     
-}
+
+
+    
+
