bdd9b23a82f594b4c45cfde95972a3fd76a78233
1Ricardo5 <rirahetahernandez25@aoiths.org>
2025-05-22 14:44:35 -0400

    Animation and Background Animation

--- Assets/Scripts/LoadMainMenu.cs
+++ Assets/Scripts/LoadMainMenu.cs
@@ -5,12 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class LoadMainMenu : MonoBehaviour
 {
+   [SerializeField] string sceneName;
+    // Assign your GameObject you want to move Scene in the Inspector
+    public GameObject m_MyGameObject;
+
    void Awake()
    {
-      LoadNewScene();
+      StartCoroutine(LoadNewScene());
    }
-   public void LoadNewScene()
+   IEnumerator LoadNewScene()
    {
-    SceneManager.LoadScene("main menu",LoadSceneMode.Additive);
+    // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        if (asyncLoad.isDone)
+        {
+          // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(m_MyGameObject, SceneManager.GetSceneByName(sceneName));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+    
+
+    
+
    }
 }
