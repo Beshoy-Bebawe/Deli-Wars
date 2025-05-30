ef89d9dcb1dd5476459be28cc6b42e8d25a1854f
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2025-05-30 15:07:45 -0400

    Some Changes
    Level 1 work

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Transition : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
-    void Update()
+    public void ChangeSceneLevel2( [SerializeField] string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
